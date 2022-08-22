3013fa52aaa94aeae4b6995f11a0863f6ab186ea
wangshushuo <shushuo.wang@77hub.com>
2022-08-22 14:48:31 +0800

    remove old meme

---
+++ themes/meme/assets/js/custom.js
@@ -0,0 +1,10 @@
+var _mtac = { "senseQuery": 1 };
+(function () {
+  var mta = document.createElement("script");
+  mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+  mta.setAttribute("name", "MTAH5");
+  mta.setAttribute("sid", "500668548");
+
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(mta, s);
+})();
